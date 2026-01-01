032bd8470222632429c8e431611420d347ea8ed7
Learnermeet <parmarmeet603@gmail.com>
2026-01-01 20:53:33 +0530

    removed warnings

--- Begineer/Calculator/Calculator.java
+++ Begineer/Calculator/Calculator.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double num1, num2, result;
